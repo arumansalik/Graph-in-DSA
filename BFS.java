794575fbcaa12be70efd7decb7a1af70da1455e6
unknown <arumansalik45@gmail.com>
2025-11-24 23:11:58 +0530

    Breadth First Search in Graph

--- BFS.java
+++ BFS.java
@@ -21,4 +21,25 @@ public class BFS {
             }
         }
     }
+
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+        graph.get(0).add(2);
+        graph.get(1).add(3);
+        graph.get(2).add(4);
+
+        graph.get(1).add(0);
+        graph.get(2).add(0);
+        graph.get(3).add(1);
+        graph.get(4).add(2);
+
+        bfs(0, graph, n);
+    }
 }
