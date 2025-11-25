644b62773b407f844f07268d79a0e60d49784a5f
unknown <arumansalik45@gmail.com>
2025-11-25 08:51:47 +0530

    Number of islands

---
+++ Problems/NumberOfIslands.java
@@ -0,0 +1,43 @@
+package Problems;
+
+public class NumberOfIslands {
+    public static int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == '1') {
+                    count++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length, cols = grid[0].length;
+        if(r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') return;
+
+        grid[r][c] = '0'; //as marked -1
+
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '0', '0'},
+                {'1', '0', '0', '1'},
+                {'0', '0', '1', '1'},
+                {'0', '0', '0', '0'}
+        };
+
+        System.out.println("Number of islands = " + numIslands(grid));
+    }
+}
