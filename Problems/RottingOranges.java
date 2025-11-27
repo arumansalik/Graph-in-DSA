6cecb67bf8b5ecc85a6bd5dff7de127cc5e93b91
unknown <arumansalik45@gmail.com>
2025-11-27 23:19:31 +0530

    Rotting Oranges

---
+++ Problems/RottingOranges.java
@@ -0,0 +1,61 @@
+package Problems;
+
+import java.util.*;
+
+public class RottingOranges {
+
+    public static int orangesRotting(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        // Add all rotten oranges initially
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 2) {
+                    q.offer(new int[]{r, c});
+                } else if (grid[r][c] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int minutes = -1;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            minutes++;
+
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+                int r = cur[0], c = cur[1];
+
+                for (int[] d : dirs) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {2,1,1},
+                {1,1,0},
+                {0,1,1}
+        };
+
+        System.out.println("Minutes to rot all oranges: " + orangesRotting(grid));
+    }
+}
