360288a2a747e57d8527c3bc26e2de09a9e0c338
unknown <arumansalik45@gmail.com>
2026-01-07 22:26:21 +0530

    Cycle Detection In Undirected Graph

--- Problems/CycleDetectionInAnUndirectedGraphUsingBFS.java
+++ Problems/CycleDetectionInAnUndirectedGraphUsingBFS.java
@@ -3,12 +3,13 @@ package Problems;
 import java.util.*;
 
 public class CycleDetectionInAnUndirectedGraphUsingBFS {
+
     static boolean detectCycle(int V, List<List<Integer>> adj) {
         boolean[] visited = new boolean[V];
 
         for (int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                if(bfsCheck(i, adj, visited)) {
+            if (!visited[i]) {
+                if (dfsCheck(i, -1, adj, visited)) {
                     return true;
                 }
             }
@@ -16,29 +17,28 @@ public class CycleDetectionInAnUndirectedGraphUsingBFS {
         return false;
     }
 
-    private static boolean bfsCheck(int start, List<List<Integer>> adj, boolean[] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start, -1});
-        visited[start] = true;
+    private static boolean dfsCheck(int node, int parent,
+                                    List<List<Integer>> adj,
+                                    boolean[] visited) {
 
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int node = curr[0];
-            int parent = curr[1];
+        visited[node] = true;
 
-            for(int neighbor: adj.get(node)) {
-                if(!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.offer(new int[]{neighbor, node});
-                } else if (neighbor != parent) {
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                if (dfsCheck(neighbor, node, adj, visited)) {
                     return true;
                 }
+            }
+            // If visited and not parent → cycle
+            else if (neighbor != parent) {
+                return true;
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
+
         int V = 5;
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
