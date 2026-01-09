182426d4cdfe4e948b40712186555da84649313b
unknown <arumansalik45@gmail.com>
2026-01-09 21:46:12 +0530

    Surronded Regions using the Queue-based Boundary Flood Fill

--- Problems/SurroundedRegions.java
+++ Problems/SurroundedRegions.java
@@ -3,41 +3,54 @@ package Problems;
 import java.util.*;
 
 public class SurroundedRegions {
+
     public static void solve(char[][] board) {
-        int m = board.length, n = board[0].length;
 
+        int m = board.length;
+        int n = board[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        // Step 1: Add boundary 'O's to queue
         for (int i = 0; i < m; i++) {
-            if(board[i][0] == 'O') dfs(board, i, 0);
-            if(board[i][n-1] == 'O') dfs(board, i, n - 1);
+            if (board[i][0] == 'O') queue.offer(new int[]{i, 0});
+            if (board[i][n - 1] == 'O') queue.offer(new int[]{i, n - 1});
         }
 
-        for (int j =0; j < n; j++) {
-            if(board[0][j] == 'O') dfs(board, 0, j);
-            if(board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') queue.offer(new int[]{0, j});
+            if (board[m - 1][j] == 'O') queue.offer(new int[]{m - 1, j});
         }
 
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // Step 2: BFS to mark safe regions
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int r = cell[0];
+            int c = cell[1];
+
+            if (r < 0 || c < 0 || r >= m || c >= n || board[r][c] != 'O')
+                continue;
+
+            board[r][c] = 'S'; // mark safe
+
+            for (int[] d : dirs) {
+                queue.offer(new int[]{r + d[0], c + d[1]});
+            }
+        }
+
+        // Step 3: Flip surrounded regions
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                if(board[i][j] == 'S') board[i][j] = 'O';
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'S') board[i][j] = 'O';
             }
         }
     }
 
-    public static void dfs(char[][] board, int i, int j) {
-        int m = board.length, n = board[0].length;
-
-        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') return;
-
-        board[i][j] = 'S';
-
-        dfs(board, i + 1, j);
-        dfs(board, i - 1, j);
-        dfs(board, i, j + 1);
-        dfs(board, i, j - 1);
-    }
-
     public static void main(String[] args) {
+
         char[][] board = {
                 {'X','X','X','X'},
                 {'X','O','O','X'},
